7613c2752e7208936518751a9a2afca9ca856bad
Diego Dmitry <diegodmitry@hotmail.com>
2021-10-15 18:42:49 -0300

    the basket challenge was done.

---
+++ modulo1_Fundamentos/bloco04_IntroJAVASCRIPT_/4.4/basket.js
@@ -0,0 +1,37 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+const quantity = (array) => {
+
+  // 1. counting the number of elements in array 
+  const abacate = array.filter(el => el === 'Abacate').length;
+  const banana = array.filter(el => el === 'Banana').length;
+  const jaca = array.filter(el => el === 'Jaca').length;
+  const laranja = array.filter(el => el === 'Laranja').length;
+  const melancia = array.filter(el => el === 'Melancia').length;
+  const pera = array.filter(el => el === 'Pera').length;
+  const uva = array.filter(el => el === 'Uva').length;
+
+  return `{Abacate: ${abacate}, Banana: ${banana}, Jaca: ${jaca}, Laranja: ${laranja}, Melancia: ${melancia}, Pera: ${pera}, Uva: ${uva}}`;
+  
+  
+
+};
+console.log(quantity(basket))
