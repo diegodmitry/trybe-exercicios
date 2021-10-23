484786d6e9682e78593e344e3c7e24b276762bb3
Diego Dmitry <diegodmitry@hotmail.com>
2021-10-22 21:27:38 -0300

    Exercises 3 and 4 was finished.

--- modulo1_Fundamentos/bl05_DOMEventsWebStor/5.3/script.js
+++ modulo1_Fundamentos/bl05_DOMEventsWebStor/5.3/script.js
@@ -1,64 +1,116 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
-    const dayListItem = document.createElement('li');
+    const dayListItem = document.createElement("li");
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
 // 1.
 const makeLi = (array) => {
-  const ulDays = document.querySelector('#days');
+  const ulDays = document.querySelector("#days");
   for (let i = 0; i < array.length; i++) {
     // creating li
-    const liDays = document.createElement('li');
-  
+    const liDays = document.createElement("li");
+
     // add id
-    liDays.id = 'days';
+    liDays.id = "days";
 
     // add class
-    liDays.classList.add('day');
-    
+    liDays.classList.add("day");
+
     // adding element of array
     liDays.innerText = array[i];
-    
+
     // add class holiday
-    if(liDays.textContent === '24' || liDays.textContent === '25' || liDays.textContent === '31') liDays.classList.add('holiday');
+    if (
+      liDays.textContent === "24" ||
+      liDays.textContent === "25" ||
+      liDays.textContent === "31"
+    )
+      liDays.classList.add("holiday");
 
     // add class friday
-    if(liDays.textContent === '4' || liDays.textContent === '11' || liDays.textContent === '18' || liDays.textContent === '25') liDays.classList.add('friday');
+    if (
+      liDays.textContent === "4" ||
+      liDays.textContent === "11" ||
+      liDays.textContent === "18" ||
+      liDays.textContent === "25"
+    )
+      liDays.classList.add("friday");
 
     // putting li inside of ul
     ulDays.appendChild(liDays);
   }
 };
-makeLi(dezDaysList)
+makeLi(dezDaysList);
 
 // 2.
+// create button
+const btnHoliday = document.createElement("button");
 const holiday = (feriados) => {
-  // create button
-  const btnHoliday = document.createElement('button');
-  
-  
   // add id
-  btnHoliday.id = 'btn-holiday';
-  
+  btnHoliday.id = "btn-holiday";
+
   // add value
   btnHoliday.textContent = feriados;
 
   // add btnHoliday to DOM
-  document.querySelector('.buttons-container').appendChild(btnHoliday);
+  document.querySelector(".buttons-container").appendChild(btnHoliday);
 
+  // add click event
+  // 3.
+  btnHoliday.addEventListener("click", function(e) {
+    const holidays = document.querySelectorAll(".holiday");
+    for (let i = 0; i < holidays.length; i++) {
+    if(holidays[i].style.backgroundColor === "") holidays[i].style.backgroundColor = rgb(172, 224, 171);
+      else {
+        holidays[i].style.backgroundColor = ""
+      }
+    }
+  });
+};
+holiday("feriados");
+
+function rgb(r, g, b) {
+  // console.log(r, g, b);
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+// 4.
+const friday = (string) => {
+  // create button
+  const btnFriday = document.createElement('button');
+
+  // button name
+  btnFriday.textContent = string;
+
+  // button id
+  btnFriday.id = "btn-friday";
+
+  // add btnFriday to DOM
+  document.querySelector(".buttons-container").appendChild(btnFriday);
 
 };
-holiday('feriados')
+friday('Sexta-feira')
