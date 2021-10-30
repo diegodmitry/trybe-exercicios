392663099e667f05f2317d5d9202370a1d89865a
Diego Dmitry <diegodmitry@hotmail.com>
2021-10-30 16:03:51 -0300

    Added just-validare library & function to validate CPF

--- modulo1_Fundamentos/bl06-html-css-forms-flexbox-responsivo/6.2/script.js
+++ modulo1_Fundamentos/bl06-html-css-forms-flexbox-responsivo/6.2/script.js
@@ -68,13 +68,9 @@ const checkInputs = () => {
 
   // const arrDates = date.value.split("/");
 
-  if (usernameValue === "") {
-    status = false;
-  } else if (emailValue === "") {
-    status = false;
-  } else if (cpfValue === "") {
-    status = false;
-  } else if (enderecoValue === "") {
+  status = isValidCPF(cpfValue)
+
+  if (enderecoValue === "") {
     status = false;
   } else if (cidadeValue === "") {
     status = false;
@@ -104,7 +100,7 @@ const checkInputs = () => {
   return status;
 };
 
-// To get the HTML form data
+// // To get the HTML form data
 const divForm = () => {
   const obj = {};
   obj.name = form.children[0].children[1].value;
@@ -140,8 +136,8 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-// check which radio btn is selected
-// using ternary operator
+// // check which radio btn is selected
+// // using ternary operator
 const isCheckedRadioBtn = (item) =>
   item[12].checked ? item[12].value : item[14].value;
 
@@ -170,3 +166,41 @@ window.addEventListener("DOMContentLoaded", () => {
   const inputDate = document.getElementById("date-input");
   inputDate.DatePickerX.init({ format: "dd/mm/yyyy" });
 });
+
+// Check if is a valid CPF
+// source: https://gist.github.com/joaohcrangel/8bd48bcc40b9db63bef7201143303937
+function isValidCPF(cpf) {
+  // Validar se é String
+  if (typeof cpf !== 'string') return false
+  
+  // Tirar formatação
+  cpf = cpf.replace(/[^\d]+/g, '')
+  
+  // Validar se tem tamanho 11 ou se é uma sequência de digitos repetidos
+  if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false
+  
+  // String para Array
+  cpf = cpf.split('')
+  
+  const validator = cpf
+      // Pegar os últimos 2 digitos de validação
+      .filter((digit, index, array) => index >= array.length - 2 && digit)
+      // Transformar digitos em números
+      .map( el => +el )
+      
+  const toValidate = pop => cpf
+      // Pegar Array de items para validar
+      .filter((digit, index, array) => index < array.length - pop && digit)
+      // Transformar digitos em números
+      .map(el => +el)
+  
+  const rest = (count, pop) => (toValidate(pop)
+      // Calcular Soma dos digitos e multiplicar por 10
+      .reduce((soma, el, i) => soma + el * (count - i), 0) * 10) 
+      // Pegar o resto por 11
+      % 11 
+      // transformar de 10 para 0
+      % 10
+      
+  return !(rest(10,2) !== validator[0] || rest(11,1) !== validator[1])
+}
