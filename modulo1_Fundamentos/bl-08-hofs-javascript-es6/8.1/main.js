dbd762647aebc0bd5b005a58a566d78fdb1034be
Diego Dmitry <diegodmitry@hotmail.com>
2021-11-17 21:21:49 -0300

    All tasks was done.

--- modulo1_Fundamentos/bl-08-hofs-javascript-es6/8.1/main.js
+++ modulo1_Fundamentos/bl-08-hofs-javascript-es6/8.1/main.js
@@ -7,16 +7,16 @@ const objFunc = (fullName) => {
   };
 };
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: objFunc('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: objFunc('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: objFunc('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   };
   return employees;
 };
 
-// console.log(newEmployees());
+// console.log(newEmployees(objFunc));
 ///////////////////////////////////////////////////////////////////////////////
 
 // 2.
