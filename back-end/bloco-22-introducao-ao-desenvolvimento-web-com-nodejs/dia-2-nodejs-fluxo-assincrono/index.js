0e3976aa7dd9ce60f21ef2389455863e4bc473e4
Diego Dmitry <diegodmitry@hotmail.com>
2022-04-14 20:21:55 -0300

    feat: added 1 task

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -0,0 +1,21 @@
+const calc = (a, b, c) => {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') reject("Informe apenas números");
+
+        const result = (a + b) * c;
+        if (result < 50) reject("Valor muito baixo");
+        if (result > 50) {
+            resolve(result);
+        }
+    });
+    
+};
+const run = async () => {
+    try {
+        console.log(calc(10, 60, 2));
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+run();
